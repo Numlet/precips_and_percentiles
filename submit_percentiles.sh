eab08ab0297bbc79ca76945a5bd9ad489c3c4330
jvergara <jvergara@env.ethz.ch>
2019-07-03 11:20:35 +0200

    splitted percentile calculations to 1 perc per file

--- submit_percentiles.sh
+++ submit_percentiles.sh
@@ -1,10 +1,14 @@
 
 partA_ID=$(sbatch --parsable create_full_data_files.sh)
-sbatch --parsable --dependency=afterany:${partA_ID} create_6min_percentiles.sh
-sbatch --parsable --dependency=afterany:${partA_ID} create_6min_percentiles_fine.sh
+sbatch --parsable --dependency=afterany:${partA_ID} create_6min_percentiles_99.sh
+sbatch --parsable --dependency=afterany:${partA_ID} create_6min_percentiles_999.sh
+sbatch --parsable --dependency=afterany:${partA_ID} create_6min_percentiles_9999.sh
+sbatch --parsable --dependency=afterany:${partA_ID} create_6min_percentiles_99999.sh
 partB_ID=$(sbatch --parsable --dependency=afterany:${partA_ID} create_timselsum_12min.sh)
-sbatch --parsable --dependency=afterany:${partB_ID} create_12min_percentiles.sh
-sbatch --parsable --dependency=afterany:${partB_ID} create_12min_percentiles_fine.sh
+sbatch --parsable --dependency=afterany:${partB_ID} create_12min_percentiles_99.sh
+sbatch --parsable --dependency=afterany:${partB_ID} create_12min_percentiles_999.sh
+sbatch --parsable --dependency=afterany:${partB_ID} create_12min_percentiles_9999.sh
+sbatch --parsable --dependency=afterany:${partB_ID} create_12min_percentiles_99999.sh
 part_B_ID=$(sbatch --parsable --dependency=afterany:${partA_ID} create_timselsum_18min.sh)
 sbatch --parsable --dependency=afterany:${partB_ID} create_18min_percentiles.sh
 sbatch --parsable --dependency=afterany:${partB_ID} create_18min_percentiles_fine.sh
