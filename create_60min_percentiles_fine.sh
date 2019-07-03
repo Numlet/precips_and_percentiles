ca0028d618e49535f28e575e6b6d1cbe7a5b9387
jvergara <jvergara@env.ethz.ch>
2019-07-03 11:24:13 +0200

    added fine percentiles in separate file

---
+++ create_60min_percentiles_fine.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -l
+#
+#SBATCH --account=pr04
+#SBATCH --time=24:00:00
+#SBATCH --job-name=percentiles60min
+#SBATCH --nodes=1
+#SBATCH --output=percentiles60.out
+#SBATCH --constraint=gpu
+
+
+#cdo -yseaspctl,99.0 full_array_60min.nc -yseasmin full_array_60min.nc -yseasmax full_array_60min.nc full_array_60min_perc_99.nc
+#cdo -yseaspctl,99.9 full_array_60min.nc -yseasmin full_array_60min.nc -yseasmax full_array_60min.nc full_array_60min_perc_999.nc
+cdo -yseaspctl,99.99 full_array_60min.nc -yseasmin full_array_60min.nc -yseasmax full_array_60min.nc full_array_60min_perc_9999.nc
+cdo -yseaspctl,99.999 full_array_60min.nc -yseasmin full_array_60min.nc -yseasmax full_array_60min.nc full_array_60min_perc_99999.nc
